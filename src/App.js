b954f94ac423bd35a2385092bc190c1093f2f4e8
unknown <ayt.ullah.dev@gmail.com>
2022-06-07 18:54:45 +0600

    React-router and Home page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="">
-        <div className='text-black text-xl'>
-           Welcome to our React App.
+    <div className="  bg-gradient-to-r from-cyan-500 to-blue-500 text-white min-w-screen min-h-screen flex justify-center items-center">
+        <div className="text-3xl p-5 w-[450px] backdrop-blur-xl rounded-lg bg-white/30 uppercase">
+             <h1 className="text-center">Welcome To Friendly Chat</h1>
+             <button className="btn bg-blue-600 hover:bg-blue-500 border-none mx-auto block my-10">CONTINUE</button>
         </div>
     </div>
   );
---
+++ src/Firebase.init.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAU123VBAHI60EEUQ75c6ftgn4BeBRsFmg",
+  authDomain: "frindlychat-ac7b8.firebaseapp.com",
+  projectId: "frindlychat-ac7b8",
+  storageBucket: "frindlychat-ac7b8.appspot.com",
+  messagingSenderId: "603969907381",
+  appId: "1:603969907381:web:becd2d826770d5c369acfc",
+  measurementId: "G-D74DL7QNHS"
+};
+
+const app = initializeApp(firebaseConfig);
