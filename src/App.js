7d90c6857bf5984be7520e4bd2fd021cd06257ae
unknown <ayt.ullah.dev@gmail.com>
2022-06-07 19:01:17 +0600

    Home page and Notfound page added with Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Notfound from "./Components/Error/Notfound";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <div className="  bg-gradient-to-r from-cyan-500 to-blue-500 text-white min-w-screen min-h-screen flex justify-center items-center">
-        <div className="text-3xl p-5 w-[450px] backdrop-blur-xl rounded-lg bg-white/30 uppercase">
-             <h1 className="text-center">Welcome To Friendly Chat</h1>
-             <button className="btn bg-blue-600 hover:bg-blue-500 border-none mx-auto block my-10">CONTINUE</button>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Notfound/>}/>
+        </Routes>
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="text-3xl p-5 w-[450px] backdrop-blur-xl rounded-lg bg-white/30 uppercase">
+        <h1 className="text-center">Welcome To Friendly Chat</h1>
+        <button className="btn bg-blue-600 hover:bg-blue-500 border-none mx-auto block my-10">
+          CONTINUE
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
